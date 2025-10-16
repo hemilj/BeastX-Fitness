90d6e72708897ebf073da4f850425b97283dc159
hemilj <hemiljariwala2005@gmail.com>
2025-10-16 10:24:34 +0530

    Add files via upload
    Updated project files

--- admin/pages/p_view.php
+++ admin/pages/p_view.php
@@ -1,80 +1,4 @@
 <?php
-//include("header.php");
-
-?>
-<?php
-// include("connectdb.php");
-// $q = mysqli_query($conn, "select * from product_master");
-// echo "<table border=2 class='table table-stripted'>";
-// echo "<tr>";
-// echo "<th>Product Name";
-// echo "<th>Photo";
-// echo "<th>Rate";
-// echo "<th>Qty";
-// echo "<th>Category Name";
-// echo "<th colspan=2>Modification";
-// echo "</tr>";
-// while ($row = mysqli_fetch_array($q)) {
-//     echo "<tr align=center>";
-//     echo "<td>" . $row['pname'];
-//     echo "<td><img src='./images/product/$row[photo]' height=100px width=100px/>";
-//     echo "<td>" . $row['rate'];
-//     echo "<td>" . $row['qty'];
-//     echo "<td><a href='delete.php?x=$row[0]'><img src='./images/trash.png' height=50px width=50px/></a>";
-//     echo "<td><a href='edit.php?x=$row[0]'><img src='./images/updated.png' height=50px width=50px/></a>";
-//     echo "</tr>";
-// }
-// echo "</table>";
-?>
-<?php
-// include("footer.php");
-?>
-
-<?php
-// include("header.php");
-// include("connectdb.php");
-
-// $q = mysqli_query($conn, "select * from product_master");
-
-// echo "<table border=2 class='table table-stripted'>";
-// echo "<tr>";
-// echo "<th>Product Name</th>";
-// echo "<th>Photo</th>";
-// echo "<th>Rate</th>";
-// echo "<th>Qty</th>";
-// echo "<th>Category Name</th>";
-// echo "<th colspan=2>Modification</th>";
-// echo "</tr>";
-
-// while ($row = mysqli_fetch_array($q)) {
-//     // Sanitize folder name same way as in upload code
-//     $folderName = preg_replace('/[^A-Za-z0-9 _-]/', '', $row['pname']);
-//     $imagePath = "./images/product/" . $folderName . "/" . $row['photo'];
-
-//     echo "<tr align='center'>";
-//     echo "<td>" . htmlspecialchars($row['pname']) . "</td>";
-
-//     // Check if image exists before showing
-//     if (file_exists($imagePath)) {
-//         echo "<td><img src='$imagePath' height='100' width='100'/></td>";
-//     } else {
-//         echo "<td><img src='./images/no-image.png' height='100' width='100'/></td>";
-//     }
-
-//     echo "<td>" . htmlspecialchars($row['rate']) . "</td>";
-//     echo "<td>" . htmlspecialchars($row['qty']) . "</td>";
-
-//     echo "<td><a href='delete.php?x=" . $row[0] . "'><img src='./images/trash.png' height='50' width='50'/></a></td>";
-//     echo "<td><a href='edit.php?x=" . $row[0] . "'><img src='./images/updated.png' height='50' width='50'/></a></td>";
-//     echo "</tr>";
-// }
-
-// echo "</table>";
-
-// include("footer.php");
-?>
-
-<?php
 include("header.php");
 include("connectdb.php");
 
@@ -108,7 +32,7 @@ while ($row = mysqli_fetch_assoc($q)) {
     $categoryFolder = preg_replace('/[^A-Za-z0-9 _-]/', '', $categoryName);
 
     // Build image path
-    $imagePath = "./images/product/" . $categoryFolder . "/" . $photo;
+    $imagePath = "./images/product/" . $photo;
 
     echo "<tr align='center'>";
     echo "<td>" . htmlspecialchars($productName) . "</td>";
